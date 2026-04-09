aa1780c44adbae757f6425a4b100046880e1e329
Samila Porto <profsamilaporto@gmail.com>
2026-04-09 08:24:38 -0300

    fix: update open-next.config.ts with required cloudflare overrides

--- open-next.config.ts
+++ open-next.config.ts
@@ -2,6 +2,15 @@ import type { OpenNextConfig } from "@opennextjs/cloudflare";
 
 const config: OpenNextConfig = {
   default: {
+    override: {
+      wrapper: "cloudflare-node",
+      converter: "edge",
+      proxyExternalRequest: "fetch",
+      incrementalCache: "dummy",
+      tagCache: "dummy",
+      queue: "dummy",
+
+    },
   },
 };
 
